50dc96b515856ffbf3a918307854090891c8fa58
lou <dustyfingers01257@gmail.com>
2021-05-01 14:51:08 -0600

    began work on custom app widget

---
+++ lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// MaterialApp(
+//     home: Scaffold(
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {},
+//         child: Icon(Icons.add),
+//       ),
+//       appBar: AppBar(
+//         title: Text('Images App!'),
+//       ),
+//     ),
+//   );
